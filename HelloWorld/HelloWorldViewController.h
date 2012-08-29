f2a1ec35343a1b13bb72174f4a87f4cd0a9f8ba4
Kirk True <kirk@mustardgrain.com>
2012-08-28 20:23:17 -0700

    First commit where it appears to be working.

--- HelloWorld/HelloWorldViewController.h
+++ HelloWorld/HelloWorldViewController.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface HelloWorldViewController : UIViewController
-
+@property(copy, nonatomic) NSString *userName;
 @end
--- HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorldViewController.m
@@ -9,11 +9,15 @@
 #import "HelloWorldViewController.h"
 
 @interface HelloWorldViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 - (IBAction)changeGreeting:(id)sender;
 
 @end
 
 @implementation HelloWorldViewController
+@synthesize label;
+@synthesize textField;
 
 - (void)viewDidLoad
 {
@@ -23,6 +27,8 @@
 
 - (void)viewDidUnload
 {
+    [self setTextField:nil];
+    [self setLabel:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -33,5 +39,21 @@
 }
 
 - (IBAction)changeGreeting:(id)sender {
+    self.userName = self.textField.text;
+    NSString *nameString = self.userName;
+    
+    if ([nameString length] == 0) {
+        nameString = @"World";
+    }
+    
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
+    self.label.text = greeting;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
+    if (theTextField == self.textField) {
+        [theTextField resignFirstResponder];
+    }
+    return YES;
 }
 @end
